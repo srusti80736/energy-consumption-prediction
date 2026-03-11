ccb31b3e92e195e1b08ca899c833af83f92fb372
srusti80736 <srustikamtar@gmail.com>
2026-03-11 20:06:32 +0530

    Configure Flask app to use dynamic port
    Update Flask app to run on specified port from environment variable.

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request
 import pickle
 import numpy as np
@@ -45,3 +46,5 @@ def predict():
 # Run server
 if __name__ == "__main__":
     app.run(debug=True)
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
